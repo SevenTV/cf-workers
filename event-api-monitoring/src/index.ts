b28973c7014fd3cbb6e7aaef13af138b556690f7
Troy Benson <troybensonsa@gmail.com>
2024-10-29 01:15:43 +0000

    add workspace glue

--- event-api-monitoring/src/index.ts
+++ event-api-monitoring/src/index.ts
@@ -1,32 +1,22 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-const VERSION = "1.0.0";
+// We should get this from the package.json
+const VERSION = await import('../package.json').then((pkg) => pkg.version);
 const TIMEOUT = 1000 * 5;
 
 export default {
 	async fetch(request, _env, _ctx): Promise<Response> {
 		const url = new URL(request.url);
 
-		if (request.method !== "GET" || url.pathname !== "/") {
+		if (request.method !== 'GET' || url.pathname !== '/') {
 			return new Response(null, { status: 404 });
 		}
 
-		return await checkWebsocketHealth().then(() => {
-			return new Response(null, { status: 204 });
-		}).catch((err) => {
-			return new Response(err, { status: 500 });
-		});
+		return await checkWebsocketHealth()
+			.then(() => {
+				return new Response(null, { status: 204 });
+			})
+			.catch((err) => {
+				return new Response(err, { status: 500 });
+			});
 	},
 } satisfies ExportedHandler<Env>;
 
@@ -35,7 +25,7 @@ function checkWebsocketHealth(): Promise<string | null> {
 	return new Promise((resolve, reject) => {
 		const socket = new WebSocket(`wss://events.7tv.io/v3?app=7tv-monitoring&version=${VERSION}`);
 
-		socket.addEventListener("message", (msg) => {
+		socket.addEventListener('message', (msg) => {
 			const json = JSON.parse(msg.data.toString());
 
 			if (json.op === 1 || json.op === 2) {
@@ -49,7 +39,7 @@ function checkWebsocketHealth(): Promise<string | null> {
 
 		setTimeout(() => {
 			// Unhealthy
-			reject("timeout");
+			reject('timeout');
 		}, TIMEOUT);
 	});
 }
