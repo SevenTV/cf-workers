a4ff3bc540b8a9f596bc3aecf0ce4ffbd2391873
Lennart Kloock <lennart.kloock@protonmail.com>
2024-10-29 01:23:30 +0100

    feat: add event-api ws monitoring

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,45 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const VERSION = "1.0.0";
+const TIMEOUT = 1000 * 5;
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, _env, _ctx): Promise<Response> {
+		const url = new URL(request.url);
+
+		if (request.method !== "GET" || url.pathname !== "/") {
+			return new Response(null, { status: 404 });
+		}
+
+		return await checkWebsocketHealth().then(() => {
+			return new Response(null, { status: 204 });
+		}).catch((err) => {
+			return new Response(err, { status: 500 });
+		});
 	},
 } satisfies ExportedHandler<Env>;
+
+function checkWebsocketHealth(): Promise<string | null> {
+	// anti-pattern but necessary here
+	return new Promise((resolve, reject) => {
+		const socket = new WebSocket(`wss://events.7tv.io/v3?app=7tv-monitoring&version=${VERSION}`);
+
+		socket.addEventListener("message", (msg) => {
+			const json = JSON.parse(msg.data.toString());
+
+			if (json.op === 1 || json.op === 2) {
+				// Healthy
+				resolve(null);
+			} else {
+				// Unhealthy
+				reject(`invalid message: ${json}`);
+			}
+		});
+
+		setTimeout(() => {
+			// Unhealthy
+			reject("timeout");
+		}, TIMEOUT);
+	});
+}
